5b671818469281108ea13f10468fdb91807f83ee
Quang Tran <qtran1018@gmail.com>
2026-01-05 05:02:44 -0500

    add weather route

--- static-data-service/app/api/v1/routes.py
+++ static-data-service/app/api/v1/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.geocoding.routes import router as geocode_router
+from app.weather_historical.routes import router as weather_router
 
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(geocode_router)
+router.include_router(weather_router)
