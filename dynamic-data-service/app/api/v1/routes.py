40544350c249bce57981d89c05e04e8f5fe98c69
Quang Tran <qtran1018@gmail.com>
2026-01-09 02:21:55 -0500

    added fxrates router

--- dynamic-data-service/app/api/v1/routes.py
+++ dynamic-data-service/app/api/v1/routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.weather_forecast.routes import router as weather_forecast_router
+from app.fx_rates.routes import router as fx_rates_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(weather_forecast_router)
+router.include_router(fx_rates_router)
