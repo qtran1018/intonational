84fa3de3048a9e12098dc894f0d026df6a9e4722
Quang Tran <qtran1018@gmail.com>
2026-03-29 02:32:07 -0400

    moved index creation to the main file instead of the repository files

--- static-data-service/app/main.py
+++ static-data-service/app/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.shared.http import init_client, close_client
+from app.weather_historical.repository import historical_weather
+from app.geocoding.repository import geocodes
+from app.api.v1.routes import router as static_data_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_client()  # startup
+    await geocodes.create_index([("inserted_on", 1)], expireAfterSeconds=31536000) #1 year expiry
+    await historical_weather.create_index([("inserted_on", 1)], expireAfterSeconds=31536000) #1 year expiry
     yield
     await close_client()  # shutdown
 
 app = FastAPI(lifespan=lifespan)
 
 # include routers here
-from app.api.v1.routes import router as static_data_router
 app.include_router(static_data_router)
