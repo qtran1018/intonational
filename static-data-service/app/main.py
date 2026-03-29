174ef53d4cecb7a56baa97cc24cee2030eb1e7bf
Quang Tran <qtran1018@gmail.com>
2026-03-29 10:59:31 -0400

    moved advisory index and added logging

--- static-data-service/app/main.py
+++ static-data-service/app/main.py
@@ -3,11 +3,26 @@ from contextlib import asynccontextmanager
 from app.shared.http import init_client, close_client
 from app.weather_historical.repository import historical_weather
 from app.geocoding.repository import geocodes
+from app.advisories.repository import advisories
 from app.api.v1.routes import router as static_data_router
+import logging
+
+# Only show WARNING+ for third-party libs
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
+logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
+logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
+
+# Show INFO+ for your app
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_client()  # startup
+    await advisories.create_index([("inserted_on", 1)], expireAfterSeconds=15768000) #6 month expiry
     await geocodes.create_index([("inserted_on", 1)], expireAfterSeconds=31536000) #1 year expiry
     await historical_weather.create_index([("inserted_on", 1)], expireAfterSeconds=31536000) #1 year expiry
     yield
